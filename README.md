f046917cb4babfbaf8d352d1a66d082301463ac0
mcharipar <mcharipar5@gmail.com>
2022-02-15 21:10:23 -0600

    add_basic_GUI_and_improve_login_interface

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 Python package to simplify working with the [bootcampspot.com](https://bootcampspot.com) api.
 
 This is a work in progress and looking to add additional features.
-- [ ] GUI
+- [x] GUI
+    - [ ] Update UI, add more interactivity for below features.
 - [ ] Create executable file for major OS's and a simple command to compile the executable on any machine capable of running [Anaconda](https://docs.anaconda.com/anaconda/install/index.html).
     - [ ] Possible Docker Containter creation.
 - [ ] Publish to [Pypi](https://pypi.org/) &/or to [Conda Forge](https://conda-forge.org/). 
-- [ ] Easier way to accept BCS login credentials and to save or update them across application uses.
+- [x] ~Easier way to accept BCS login credentials and to save or update them across application uses.~
 - [ ] Allows user to select which course(s) to return data on, if multiple courses tied to single account.
 - [ ] Saves and opens PDF report on course metrics in users default browser.
     - [ ] Univariate & Multivariate forecasting students potential performance.
--- testscript.py
+++ testscript.py
@@ -43,10 +43,116 @@ bcs_username = os.getenv("BCS_USERNAME")
 bcs_password = os.getenv("BCS_PASSWORD")
 course_number = os.getenv("COURSE_NUMBER")
 
-# Add course_number arguement
+# MAKE TO ACCEPT course_number as arguement       
 bcs_api_obj = bcs.BCS_API(bcs_username, bcs_password)
-print(bcs_api_obj.headers)
-print(bcs_api_obj.course_id)
-print(bcs_api_obj.attendance)
-print(bcs_api_obj.grades)
-print(bcs_api_obj.bcs_weekly_feeback())
+
+layout = [  [sg.Text('BCS Output')],
+            [sg.MLine(key='-ML1-'+sg.WRITE_ONLY_KEY, size=(75,8))],
+            [sg.MLine(key='-ML2-'+sg.WRITE_ONLY_KEY,  size=(75,8))],
+            [sg.MLine(key='-ML3-'+sg.WRITE_ONLY_KEY,  size=(75,8))],
+            [sg.MLine(key='-ML4-'+sg.WRITE_ONLY_KEY,  size=(75,8))],
+            # [sg.MLine(key='-ML5-'+sg.WRITE_ONLY_KEY,  size=(75,8))],
+            # [sg.Button('Go'), sg.Button('Exit')]]
+            [sg.Button('Exit')]]
+
+
+window = sg.Window('Window Title', layout, finalize=True)
+
+
+# Note, need to finalize the window above if want to do these prior to calling window.read()
+window['-ML1-'+sg.WRITE_ONLY_KEY].print('AuthToken:\n', end='', font='Courier 12', text_color='red', background_color='yellow')
+window['-ML1-'+sg.WRITE_ONLY_KEY].print(bcs_api_obj.headers,'\n',end='')
+window['-ML1-'+sg.WRITE_ONLY_KEY].print('Course ID:\n', end='', font='Courier 12',text_color='white', background_color='green')
+window['-ML1-'+sg.WRITE_ONLY_KEY].print(bcs_api_obj.course_id,end='')
+
+window['-ML2-'+sg.WRITE_ONLY_KEY].print('Attendance:\n', end='', font='Courier 12',text_color='white', background_color='green')
+window['-ML2-'+sg.WRITE_ONLY_KEY].print(bcs_api_obj.attendance,end='')
+
+window['-ML3-'+sg.WRITE_ONLY_KEY].print('Grades:\n', end='', font='Courier 12',text_color='white', background_color='green')
+window['-ML3-'+sg.WRITE_ONLY_KEY].print(bcs_api_obj.grades,end='')
+
+window['-ML4-'+sg.WRITE_ONLY_KEY].print('Feedback\n', end='', font='Courier 12',text_color='white', background_color='green')
+window['-ML4-'+sg.WRITE_ONLY_KEY].print(bcs_api_obj.bcs_weekly_feeback(),end='')
+
+# counter = 0
+
+while True:             # Event Loop
+    event, values = window.read(timeout=100)
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    # if event == 'Go':
+    #     window['-ML1-'+sg.WRITE_ONLY_KEY].print(event, values, text_color='red')
+    # window['-ML2-'+sg.WRITE_ONLY_KEY].print(counter)
+    # counter += 1
+window.close()
+
+
+
+# REFERENCE
+# https://pysimplegui.readthedocs.io/en/latest/cookbook/#recipe-1-shot-window-simple-data-entry-return-values-auto-numbered
+
+# print = sg.Print
+
+# sg.Print('Re-routing the stdout', do_not_reroute_stdout=False)
+# print('This is a normal print that has been re-routed.')
+
+# Add course_number arguement
+# bcs_api_obj = bcs.BCS_API(bcs_username, bcs_password)
+# print(bcs_api_obj.headers)
+# print(bcs_api_obj.course_id)
+# print(bcs_api_obj.attendance)
+# print(bcs_api_obj.grades)
+# print(bcs_api_obj.bcs_weekly_feeback())
+
+
+# sg.Print(bcs_api_obj.headers, text_color='white', background_color='green', font='Courier 10')
+# sg.Print(bcs_api_obj.course_id)
+# sg.Print(bcs_api_obj.attendance)
+# sg.Print(bcs_api_obj.grades, background_color='red', text_color='white')
+# sg.Print(bcs_api_obj.bcs_weekly_feeback(), bcs_api_obj.headers, 'such as sep', sep=',')
+# sg.Print('To not extend a colored line use the "end" parm', background_color='blue', text_color='white', end='')
+# sg.Print('\nThis line has no color.')
+
+
+# layout = [  [sg.Text('What you print will display below:')],
+#             [sg.Output(size=(50,10))],# key='-OUTPUT-')],
+#             # [sg.In(key='-IN-')],
+#             [sg.Button('Go'), sg.Button('Clear'), sg.Button('Exit')]  ]
+
+# window = sg.Window('Window Title', layout)
+
+# while True:             # Event Loop
+#     event, values = window.read()
+#     # print(event, values)
+#     bcs_api_obj = bcs.BCS_API(bcs_username, bcs_password)
+#     print(bcs_api_obj.headers)
+#     print(bcs_api_obj.course_id)
+#     print(bcs_api_obj.attendance)
+#     print(bcs_api_obj.grades)
+#     print(bcs_api_obj.bcs_weekly_feeback())
+#     if event in (sg.WIN_CLOSED, 'Exit'):
+#         break
+#     if event == 'Clear':
+#         window['-OUTPUT-'].update('')
+# window.close()
+
+
+
+# layout = [[sg.VPush()],
+#           [sg.Push(), sg.Text('Centered in the window'), sg.Push()],
+#           [sg.Push(), sg.Button('Ok'), sg.Button('Cancel'), sg.Push()],
+#           [sg.VPush()]]
+
+# window = sg.Window('A Centered Layout', layout, resizable=True, size=(300, 300))
+
+# while True:
+#     event, values = window.read()
+#     # bcs_api_obj = bcs.BCS_API(bcs_username, bcs_password)
+#     print(bcs_api_obj.headers)
+#     print(bcs_api_obj.course_id)
+#     print(bcs_api_obj.attendance)
+#     print(bcs_api_obj.grades)
+#     print(bcs_api_obj.bcs_weekly_feeback())
+#     if event in (sg.WIN_CLOSED, 'Cancel'):
+#         break
+# window.close()        
